282b4e0e176197fef1db8361ffd2b3b39a1a0e12
Dann7i <hermawanwildan78@gmail.com>
2025-09-12 19:37:39 +0700

    variable

---
+++ latihan-1/variable.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nama string = "Wildan Hermawan"
+	var umur int = 17
+	kota := "Kota Cimahi"
+	tahun := 2025
+
+	fmt.Println("Nama saya ", nama)
+	fmt.Println("Umur saya ", umur)
+	fmt.Println("Saya tinggal di ", kota)
+	fmt.Println("Tahun sekarang ", tahun)
+}
